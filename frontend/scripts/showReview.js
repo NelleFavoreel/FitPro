04d5985a2acb54e56cc7ec2c51e65edbab1d139f
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2023-12-11 17:37:02 +0100

    review showing on page

---
+++ frontend/scripts/showReview.js
@@ -0,0 +1,41 @@
+// Assuming you have an element with the class "mySlides fade" where you want to display the reviews
+import Review from "./Review.js";
+
+const slidesContainer = document.querySelector(".slideshow-container");
+
+fetch("http://localhost:3000/showreview", {
+	method: "GET",
+	headers: {
+		"Content-Type": "application/json;charset=utf-8",
+	},
+})
+	.then((response) => response.json())
+	.then((data) => {
+		// Assuming data is an array of reviews
+		console.log(data);
+		data.forEach((reviewData, index) => {
+			// Create a new Review instance using the data
+			const review = new Review(reviewData.name, reviewData.age, reviewData.email, reviewData.description);
+
+			// Append the HTML string of the review to the slidesContainer
+
+			console.log(review);
+
+			const reviewHTML = `
+            <div class="mySlides fade">
+                <div class="numbertext">${index + 1} / ${data.length}</div>
+                <div class="text">
+                    <div>
+                        <h1>${review.name}</h1>
+                        <h2>${review.age}</h2>
+                        <p>${review.description}</p>
+                    </div>
+                </div>
+            </div>`;
+
+			slidesContainer.insertAdjacentHTML("beforeend", reviewHTML);
+		});
+	})
+	.catch((error) => {
+		console.error(error);
+	});
