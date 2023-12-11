027244f570d057528489af778ff048f5206be015
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2023-12-11 16:32:06 +0100

    review post working

---
+++ frontend/scripts/review.js
@@ -0,0 +1,44 @@
+"use strict";
+
+document.querySelector("#review").addEventListener("submit", reviews);
+
+function reviews(event) {
+	event.preventDefault();
+	console.log("function called");
+
+	let userName = document.querySelector("#name").value;
+	let userAge = document.querySelector("#age").value;
+	let userEmail = document.querySelector("#email").value;
+	let userDescription = document.querySelector("#review")[3].value;
+
+	const data = {
+		name: userName,
+		age: userAge,
+		email: userEmail,
+		description: userDescription,
+	};
+
+	console.log(data);
+
+	fetch("http://localhost:3000/review", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json;charset=utf-8",
+		},
+		body: JSON.stringify(data),
+	})
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Network response was not okay");
+			}
+			return response.json();
+		})
+		.then((data) => {
+			const message = data.message;
+			document.querySelector("#returnMessage").insertAdjacentHTML("beforeend", `<p>${message}</p>`);
+			console.log(`used email: ${userEmail}`);
+		})
+		.catch((error) => {
+			console.error(error);
+		});
+}
