4a646e4137712ae1071d1a4bce8c30dff1e13248
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2023-12-13 13:13:24 +0100

    Emails can be submitted trough homepage

--- frontend/scripts/resievingEmail.js
+++ frontend/scripts/resievingEmail.js
@@ -1,15 +1,30 @@
-document.querySelector("#review").addEventListener("submit", reviews);
+document.querySelector("#emailContent").addEventListener("submit", getEmail);
 
-function reviews(event) {
+function getEmail(event) {
 	event.preventDefault();
 	console.log("function called");
 
-	let email = document.querySelector("#email").value;
+	let email = document.querySelector("#emailField").value;
 
 	fetch("http://localhost:3000/getEmail", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json;charset=utf-8",
 		},
-	});
+		body: JSON.stringify({ userEmail: email }),
+	})
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Network response was not okay");
+			}
+			location.reload();
+			return response.json();
+		})
+		.then((data) => {
+			console.log(data); // Log the response from the server
+		})
+
+		.catch((error) => {
+			console.error(error);
+		});
 }
