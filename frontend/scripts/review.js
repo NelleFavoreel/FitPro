0095a51d3f0b25219b0982cf5ede0e8b4fd527de
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2023-12-20 14:23:01 +0100

    delete function working

--- frontend/scripts/review.js
+++ frontend/scripts/review.js
@@ -1,8 +1,9 @@
 export default class Review {
-	constructor(name, age, email, description) {
+	constructor(name, age, email, description, _id) {
 		this.name = name;
 		this.age = age;
 		this.email = email;
 		this.description = description;
+		this._id = _id;
 	}
 }
--- frontend/scripts/showReview.js
+++ frontend/scripts/showReview.js
@@ -9,28 +9,59 @@ fetch("http://localhost:3000/showreview", {
 	},
 })
 	.then((response) => response.json())
-	.then((data) => {
+	.then(async (data) => {
+		// Added async to the function to use 'await'
 		console.log(data);
-		data.forEach((reviewData, index) => {
-			const review = new Review(reviewData.name, reviewData.age, reviewData.email, reviewData.description);
+		data.forEach(async (reviewData, index) => {
+			// Added async to the forEach callback
+			const review = new Review(reviewData.name, reviewData.age, reviewData.email, reviewData.description, reviewData._id);
 
 			console.log(review);
 
 			const reviewHTML = `
             <div class="mySlides fade">
                 <div class="numbertext">${index + 1} / ${data.length}</div>
+				
                 <div class="text">
                     <div>
                         <h1>${review.name}</h1>
                         <h2>${review.age}</h2>
                         <p>${review.description}</p>
+						<button class="closeButton-${index}">Delete review</button>
                     </div>
                 </div>
-            </div>`;
+				
+			</div>`;
 
 			slidesContainer.insertAdjacentHTML("beforeend", reviewHTML);
-		});
 
+			// Move event listener code inside the forEach loop
+			const deleteButton = document.querySelector(`.closeButton-${index}`);
+
+			deleteButton.addEventListener("click", async function (event) {
+				try {
+					console.log(reviewData._id);
+
+					const response = await fetch(`http://localhost:3000/showreview/${reviewData._id}`, {
+						method: "DELETE",
+						headers: {
+							"Content-Type": "application/json;charset=utf-8",
+						},
+					});
+
+					if (response.ok) {
+						console.log("Review deleted successfully");
+						location.reload();
+					} else {
+						console.log(`Failed to delete review. Server returned status: ${response.status}`);
+						const errorMessage = await response.text();
+						console.error(`Error message from server: ${errorMessage}`);
+					}
+				} catch (error) {
+					console.log(error);
+				}
+			});
+		});
 		showSlides(1);
 	})
 	.catch((error) => {
